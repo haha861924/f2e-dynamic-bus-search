f4d242b96dde05d972505ca6f9c9c3e96754a9ed
Augustine <ashramwen@gmail.com>
2021-11-17 02:17:10 +0800

    feat: 🎸 Set up .env

--- src/utils/req.js
+++ src/utils/req.js
@@ -1,72 +1,70 @@
-import axios from 'axios'
-import { CONFIG } from '@/constants/config'
-import { getAuthorizationHeader } from '@/utils/getAuthorizationHeader'
+import axios from 'axios';
+import { CONFIG } from '@/constants/config';
 
-const { apiURL } = CONFIG
+const { apiURL } = CONFIG;
 
 const instance = axios.create({
   baseURL: apiURL,
-  headers: getAuthorizationHeader(),
   timeout: 20000,
-})
+});
 
 instance.interceptors.request.use(
   function (config) {
     // Do something before request is sent
     // e.g. show loading
-    return config
+    return config;
   },
   function (error) {
     // Do something with request error
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 instance.interceptors.response.use(
   function (response) {
     // Do something with response data
     // e.g. hide loading
-    return response
+    return response;
   },
   function (error) {
     if (error.response) {
       switch (error.response.status) {
         case 404:
-          console.log('你要找的頁面不存在')
-          break
+          console.log('你要找的頁面不存在');
+          break;
         case 500:
-          console.log('程式發生問題')
-          break
+          console.log('程式發生問題');
+          break;
         default:
-          console.log(error.message)
+          console.log(error.message);
       }
     }
     if (!window.navigator.onLine) {
-      alert('網路出了點問題，請重新連線後重整網頁')
-      return
+      alert('網路出了點問題，請重新連線後重整網頁');
+      return;
     }
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 function get(url, data = null, config) {
-  return instance.get(url, { params: { ...data, $format: 'JSON' }, ...config })
+  return instance.get(url, { params: { ...data, $format: 'JSON' }, ...config });
 }
 
 function post(url, data = null, config) {
-  return instance.post(url, data, config)
+  return instance.post(url, data, config);
 }
 
 function deleteRequest(url, data = null, config) {
-  return instance.delete(url, { params: data, ...config })
+  return instance.delete(url, { params: data, ...config });
 }
 
 function put(url, data = null, config) {
-  return instance.put(url, data, config)
+  return instance.put(url, data, config);
 }
 
 function patch(url, data = null, config) {
-  return instance.patch(url, data, config)
+  return instance.patch(url, data, config);
 }
 
 const req = {
@@ -75,6 +73,6 @@ const req = {
   delete: deleteRequest,
   put,
   patch,
-}
+};
 
-export default req
+export default req;
